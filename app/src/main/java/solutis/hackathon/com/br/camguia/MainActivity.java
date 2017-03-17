741e514a1dd5fab7c65e0d7d31e568b90896698e
Elvis <elvists@gmail.com>
2017-03-17 18:55:25 -0300

    capturando imagem

--- app/src/main/java/solutis/hackathon/com/br/camguia/MainActivity.java
+++ app/src/main/java/solutis/hackathon/com/br/camguia/MainActivity.java
@@ -1,13 +1,31 @@
 package solutis.hackathon.com.br.camguia;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            this.imageView = (ImageView)this.findViewById(R.id.imageView1);
+            imageView.setImageBitmap(photo);
+        }
     }
 }
