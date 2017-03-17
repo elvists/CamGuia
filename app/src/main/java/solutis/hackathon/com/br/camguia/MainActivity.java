7444db38fb2323ab9f6fd8bb0ff0f7d743fb1c42
Elvis <elvists@gmail.com>
2017-03-17 18:19:35 -0300

    Initial commit

---
+++ app/src/main/java/solutis/hackathon/com/br/camguia/MainActivity.java
@@ -0,0 +1,13 @@
+package solutis.hackathon.com.br.camguia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
