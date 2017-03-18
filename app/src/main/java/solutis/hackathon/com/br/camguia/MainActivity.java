33c3fc114819b547ef2c936377cbcf8f8c57c422
Elvis <elvists@gmail.com>
2017-03-17 21:43:08 -0300

    capturando imagem

--- app/src/main/java/solutis/hackathon/com/br/camguia/MainActivity.java
+++ app/src/main/java/solutis/hackathon/com/br/camguia/MainActivity.java
@@ -6,12 +6,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         cameraIntent.putExtra("android.intent.extra.quickCapture", true);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, "teste");
+      //  cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, "teste");
         startActivityForResult(cameraIntent, CAMERA_REQUEST);
 
         ActivityCompat.requestPermissions(this,
@@ -44,6 +48,30 @@ public class MainActivity extends AppCompatActivity {
             MediaStore.Images.Media.insertImage(getContentResolver(), photo, "teste" , "teste desc");
             this.imageView = (ImageView)this.findViewById(R.id.imageView1);
             imageView.setImageBitmap(photo);
+            createDirectoryAndSaveFile(photo, "manga");
+        }
+    }
+
+    private void createDirectoryAndSaveFile(Bitmap imageToSave, String fileName) {
+
+        File direct = new File(Environment.getExternalStorageDirectory() + "/DirName");
+
+        if (!direct.exists()) {
+            File wallpaperDirectory = new File("/sdcard/DirName/");
+            wallpaperDirectory.mkdirs();
+        }
+
+        File file = new File(new File("/sdcard/DirName/"), fileName);
+        if (file.exists()) {
+            file.delete();
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            imageToSave.compress(Bitmap.CompressFormat.JPEG, 1, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
